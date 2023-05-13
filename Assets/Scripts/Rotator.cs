c5fe46824dd9f4d93d54f5064b1ad9fa43902a0e
sgj0402 <sgj0402@gmail.com>
2023-05-14 03:33:43 +0900

    add Rotator

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 0.2f;
+    private float angle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        angle = 0f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            angle += rotateSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)) {
+            angle -= rotateSpeed;
+        }
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
