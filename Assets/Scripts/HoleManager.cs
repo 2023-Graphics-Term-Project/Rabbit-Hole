e9f348bc677d7d83adaeaa21f8466f5984ce0ee3
sgj0402 <sgj0402@gmail.com>
2023-06-03 15:47:29 +0900

    create HoleManager

---
+++ Assets/Scripts/HoleManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HoleManager : MonoBehaviour
+{
+    public bool playerHasCarrot = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PassLevel();
+        }
+    }
+
+    public void PassLevel()
+    {
+        if (!playerHasCarrot)
+        {
+            return;
+        }
+
+        if (SceneManager.GetActiveScene().name == "Map1")
+        {
+            SceneManager.LoadScene("Map2");
+        }
+        else if (SceneManager.GetActiveScene().name == "Map2")
+        {
+            SceneManager.LoadScene("Map3");
+        }
+    }
+}
