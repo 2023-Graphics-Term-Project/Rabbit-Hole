79a74d631ffc0b0174833400e2d4d696f678db77
Lia316 <kjk0825@postech.ac.kr>
2023-06-04 14:22:56 +0900

    fix: death animation
    since someone changed

--- Assets/Scripts/GameStop.cs
+++ Assets/Scripts/GameStop.cs
@@ -8,7 +8,7 @@ public class GameStop : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        animator = GameObject.FindWithTag("Player").GetComponent<Animator>();
+        animator = GameObject.FindWithTag("RabbitAnimator").GetComponent<Animator>();
     }
 
     // Update is called once per frame
