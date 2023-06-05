9f410b33bb97bc46b7b74a958ca89c7ed3110bb3
kdh0517 <105841491+kdh0517@users.noreply.github.com>
2023-06-05 11:47:12 +0900

    add gameover

--- Assets/Scripts/GameStop.cs
+++ Assets/Scripts/GameStop.cs
@@ -18,6 +18,7 @@ public class GameStop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         
     }
 
@@ -38,6 +39,7 @@ public class GameStop : MonoBehaviour
             {
                 cameraZoom.StartZoom();
             }
+            Gameoverset.SetActive(true);
         }
     }
 
